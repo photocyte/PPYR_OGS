4cfced0d3e12f91f6d7a08307741e4e72375456e
Timothy Fallon <tfallon@mit.edu>
2018-05-17 16:18:56 -0400

    typos

--- utility_scripts/FASTA_files_from_GFF3.sh
+++ utility_scripts/FASTA_files_from_GFF3.sh
@@ -2,14 +2,14 @@
 
 echo "This is a script intended to take a Official Gene Set file which is very nearly in a standardized format (e.g., you edited the existing OGS GFF3 file with a simple text editor), and reformat that GFF3 plus produce the derivative FASTA files to ensure they are in a standardized format"
 
-if [ -z "$1" ] 
+if [ -z "$1" ];
 then
 echo "First argument should be the path to the GFF3 file to process."
 echo "The naming of the GFF3 should be in the style PPYR_OGS1.0.gff3, where PPYR = the Locus Tag Prefix for the genome in question, and 1.0 = the release # you want to build, e.g. 1.0, 1.1, 1.2, 1.3... If you edited the existing GFF3 file in the repository, conviniently it should already have this naming scheme"
 exit
 fi
 
-if [ -z "$2"" ]; then
+if [ -z "$2" ]; then
     echo "Second argument should be path to the genome reference file"
     exit
 fi
