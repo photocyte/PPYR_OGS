344054049abb7ffdd4a6f8525d8a9c0c6fabca8d
Timothy Fallon <tfallon@mit.edu>
2019-02-06 23:13:21 -0500

    more script update

--- utility_scripts/sort-and-regenerate-OGS-GFF.sh
+++ utility_scripts/sort-and-regenerate-OGS-GFF.sh
@@ -20,9 +20,7 @@ BASE=$(basename $1)
 BASE=${BASE%.gff3}
 
 echo "Adding start/stop codons with GAG..."
-rm -rf gag_output
-mkfifo tmp0.gff3
-cat $1 | grep -v "stop_codon" | grep -v "start_codon" > tmp0.gff3 &
+cat $1 | grep -v "stop_codon" | grep -v "start_codon" > tmp0.gff3
 python2 /lab/solexa_weng/testtube/GAG-2.0.1/gag.py --fix_start_stop -f ./Genome_release.fa -g tmp0.gff3
 rm -f tmp0.gff3
 echo "Done adding codons"
