ed95e138225418e0cf3a475c131e9cefa0bdae12
timfallon <tfallon@mit.edu>
2019-08-06 12:40:07 -0700

    script update

--- utility_scripts/sort-and-regenerate-OGS-GFF.sh
+++ utility_scripts/sort-and-regenerate-OGS-GFF.sh
@@ -66,7 +66,7 @@ seqkit stat ${BASE}.gene.fa.gz >> fasta-checksums.txt
 md5sum ${BASE}.gene.fa.gz >> fasta-checksums.txt
 echo "#########" >> fasta-checksums.txt
 echo "Genome_release.fa" >> fasta-checksums.txt
-echo `readlink -f Genome_release.fa` >> fasta-checksums.txt
+echo `basename $(readlink -f Genome_release.fa)` >> fasta-checksums.txt
 seqkit stat Genome_release.fa >> fasta-checksums.txt
 ./utility_scripts/checksum_FASTA_files.sh Genome_release.fa >> fasta-checksums.txt
 md5sum Genome_release.fa >> fasta-checksums.txt
